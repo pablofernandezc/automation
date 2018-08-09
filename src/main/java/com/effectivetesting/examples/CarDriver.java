5b8a46622eb9bc06d3f36bdff402162b55640988
marcelo corpucci <mcorpucci@gmail.com>
2018-08-09 15:50:02 -0700

    Ejercicio resuelto.

--- src/main/java/com/effectivetesting/examples/CarDriver.java
+++ src/main/java/com/effectivetesting/examples/CarDriver.java
@@ -4,5 +4,16 @@ public class CarDriver {
 	public static void main(String[] args) {
 		Car myCar = new Car();
 		System.out.println(myCar.type);
+		
+		switch (myCar.segment) {
+		case "A":
+			System.out.println("City Car");
+			break;
+		case "B":
+			System.out.println("Compact");
+			break;
+		case "SUV":
+			System.out.println("Sport Utility Vehicle");
+		}
 	}
 }
