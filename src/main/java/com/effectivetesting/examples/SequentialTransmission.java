2a9a5c1b3e6fdd4952365bc84db01646cb39e673
marcelo corpucci <mcorpucci@gmail.com>
2018-08-14 15:50:21 -0700

    eje completo

---
+++ src/main/java/com/effectivetesting/examples/SequentialTransmission.java
@@ -0,0 +1,33 @@
+package com.effectivetesting.examples;
+
+public class SequentialTransmission implements Transmission{
+
+	private int currentGear;
+	private String mode;
+	private int maxGear=6;
+	
+	public SequentialTransmission() {
+		this.currentGear = 0;
+		this.mode= "NEUTRAL";
+	}
+	
+	public void setMode(String mode) {
+		this.mode = mode;
+		this.gearUp();
+	}
+	
+	public void gearUp() {
+		if(currentGear < maxGear) {
+			currentGear = currentGear + 1;
+		}
+	}
+
+	public void gearDown() {
+		if(currentGear >= 1)
+		currentGear = currentGear - 1;
+	}
+
+	public int showCurrentGear() {
+		return currentGear;
+	}
+}
